c6568a300cc85f4def6c050c6a473ca586b44b89
gyu716625 <gyu716625@gmail.com>
2020-07-31 17:01:40 +0900

    :recycle: Refactoring transaction
     - 유저의 정보가 지워질때, season_items 테이블에 있는 데이터가 무조건 함께 지워질 수 있도록 transaction을 적용

--- controllers/user/deleteuser.js
+++ controllers/user/deleteuser.js
@@ -9,55 +9,49 @@ module.exports= {
 
         let season_itemId = [];
 
-        await items_seasons.findAll({
-            attributes: ['items_seasons.id'],
-            include: [{
-                model: Items,
-                where: {UserId : id}
-            }],
-            raw:true
-        }).then((result) => {
-            if(result.length){
-                result.map((acc) => {
-                    season_itemId.push(acc.id);
+        try{
+            await sequelize.transaction(async(t) => {
+
+                await items_seasons.findAll({
+                    attributes: ['items_seasons.id'],
+                    include: [{
+                        model: Items,
+                        where: {UserId : id}
+                    }],
+                    raw:true,
+                    transaction: t
+                }).then((result) => {
+                    if(result.length){
+                        result.map((acc) => {
+                            season_itemId.push(acc.id);
+                        })
+                        console.log(season_itemId)
+                    }
                 })
-                console.log(season_itemId)
-            }
-        })
 
 
-        await Items.destroy({
-            where: { Userid: id }
-        })
-        
-        for(let i of season_itemId){
-            await items_seasons.destroy({
-                where:{
-                    id: i
-                }
+                await Items.destroy({
+                    where: { Userid: id }
+                },{ transaction: t })
+                
+                for(let i of season_itemId){
+                    await items_seasons.destroy({
+                        where:{
+                            id: i
+                        }
+                    },{ transaction: t })
+                }                
             })
+
+            await Users.destroy({
+                where: { id : id }
+            })
+
+            res.status(200).json({ message: 'Successful' });
+
+        }catch(err){
+            console.log(err.message)
+            res.status(404).json({ message: "Failed", err: err.message });
         }
-
-        await Users.destroy({
-            where: { id : id }
-        })
-        .then(() => {
-          res.status(200).json({ message: 'Successful' })
-        })
-        .catch((err) => {
-          console.log('delete user: ', err.message);
-          res.status(404).json({ message: 'Failed', err: err.message })
-        });
-        
-        
-
-       
-        //.then(()=> res.status(200).json({"message": "Successful"}))
-        //.catch(e => res.status(404).send({"message": "Failed", "err": e.message})) 
-
-
-        
-
-
     }
 }
